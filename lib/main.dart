bab392bfe1da69f6dd3b146302e4a7c7f00e31da
Túlio <tuliofranca@visionnaire.com.br>
2018-12-05 19:47:10 -0200

    Adiciona text field interativo Posiciona text composer widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,38 @@ class FriendlyChatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ChatScreenState();
+  }
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = TextEditingController();
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+
+  Widget _buildTextComposer() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: TextField(
+        controller: _textController,
+        onSubmitted: _handleSubmitted,
+        decoration: InputDecoration.collapsed(hintText: "Send a message"),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("FriendlyChat"),
       ),
+      body: _buildTextComposer(),
     );
   }
 }
