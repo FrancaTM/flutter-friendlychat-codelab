1c35c4d4012b1fbeabe89f2ccbb38b75169b10ff
Túlio <tuliofranca@visionnaire.com.br>
2018-12-05 23:02:26 -0200

    Implementa uma lista de mensagens

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,8 @@ class _ChatScreenState extends State<ChatScreen> {
               child: TextField(
                 controller: _textController,
                 onSubmitted: _handleSubmitted,
-                decoration: InputDecoration.collapsed(hintText: "Send a message"),
+                decoration:
+                    InputDecoration.collapsed(hintText: "Send a message"),
               ),
             ),
             Container(
@@ -62,6 +63,45 @@ class _ChatScreenState extends State<ChatScreen> {
         title: Text("FriendlyChat"),
       ),
       body: _buildTextComposer(),
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  final String text;
+
+  ChatMessage(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    const String _name = "Túlio";
+
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(
+              child: Text(_name[0]),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                _name,
+                style: Theme.of(context).textTheme.subhead,
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
